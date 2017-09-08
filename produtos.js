491d528f9294fa1e448bbdbe4e9f5ba21de7076e
VitoriaAdame <vitoriamoura423@gmail.com>
2017-09-07 21:36:10 -0300

    Add files via upload

---
+++ produtos.js
@@ -0,0 +1,29 @@
+var produtos = [
+
+	{"id":1,nome:"Expresso",preco:4.00},
+
+	{"id":2,nome:"Cappuccino",preco:5.00},
+
+	{"id":3,nome:"Frappuccino Brigadeiro ",preco:7.00},
+
+	{"id":4,nome:"Refreshers de fruts vermelhas",preco:10.00},
+
+	{"id":5,nome:"Chá gelado",preco:8.00},
+
+	{"id":6,nome:"Chocolate Clássico ",preco:4.00},
+
+	{"id":7,nome:"Frappuccino à base de café",preco:8.00},
+
+	{"id":8,nome:"Frappuccino à base de creme",preco:15.00},
+
+	{"id":9,nome:"Cheesecake",preco:7.00},
+
+	{"id":10,nome:"Cappuccino Chocolate ",preco:6.00},
+
+	{"id":11,nome:"Pão de Queijo",preco:8.00},
+
+	{"id":12,nome:"Cold Brew Tradicional",preco:10.00}
+
+	
+
+];
